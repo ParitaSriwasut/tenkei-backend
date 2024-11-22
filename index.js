8ff80deacfc2b4d143bc4361c4a11b5451671367
FILDSSS <kiangsak.r@japansystem.co.th>
2024-11-22 10:43:05 +0700

    update vercel

--- index.js
+++ index.js
@@ -60,3 +60,5 @@ app.use('/navfg', navfgRoute);
 app.use('/navwi', navwiRoute);
 const PORT = process.env.PORT || "4000";
 app.listen(PORT, () => console.log(`server running on port: ${PORT}`));
+
+module.exports = app;
