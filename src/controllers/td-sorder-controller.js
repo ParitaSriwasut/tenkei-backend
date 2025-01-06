14e71b166f683de754aacf1ef98e64da87b4fc3b
DETMON CHANGCHIT <59195286+Detmon0410@users.noreply.github.com>
2025-01-06 13:31:44 +0700

    ิbuild get SOrder api
    -customer, od_quote, currency, contract_docu

--- src/controllers/td-sorder-controller.js
+++ src/controllers/td-sorder-controller.js
@@ -52,3 +52,114 @@ exports.Search_SOrder_No_AfterUpdate = async (req, res, next) => {
     );
   }
 };
+
+
+exports.get_customer_info_SOrder = async (req, res, next) => {
+  try {
+      // Select only the required fields from the tm_customer table
+      const customer = await prisma.tM_Customer.findMany({
+          select: {
+              Customer_CD: true,
+              Customer_Abb: true,
+              Customer_Name: true,
+              Customer_Remark: true,
+          },
+      })
+
+      // Send the selected data back to the user
+      return res.status(200).json({
+          status: "success",
+          data: {
+              customer: customer,
+          },
+      })
+  } catch (err) {
+      console.error("Error searching customer:", err)
+      return next(createError(500, "Internal Server Error"))
+  }
+}
+exports.get_unit_SOrder = async (req, res, next) => {
+  try {
+      // Select only the required fields from the tm_customer table
+      const unit = await prisma.tM_Unit.findMany({
+          select: {
+              Unit_CD: true,
+              Unit_Abb: true,
+              Unit_Name: true,
+              Unit_Remark: true,
+          },
+      })
+
+      // Send the selected data back to the user
+      return res.status(200).json({
+          status: "success",
+          data: {
+            unit: unit,
+          },
+      })
+  } catch (err) {
+      console.error("Error searching unit:", err)
+      return next(createError(500, "Internal Server Error"))
+  }
+}
+
+exports.get_od_quote_SOrder = async (req, res, next) => {
+  try {
+      // Select only the required fields from the tm_customer table
+      const od_quote = await prisma.tM_Od_Quote.findMany({
+          select: {
+              Od_Quote_CD: true,
+              Od_Quote_Abb: true,
+              Od_Quote_Name: true,
+              Od_Quote_Remark: true,
+          },
+      })
+
+      // Send the selected data back to the user
+      return res.status(200).json({
+          status: "success",
+          data: {
+            od_quote: od_quote,
+          },
+      })
+  } catch (err) {
+      console.error("Error searching od quote:", err)
+      return next(createError(500, "Internal Server Error"))
+  }
+}
+
+exports.get_currency_SOrder = async (req, res, next) => {
+  try {
+     
+    const currency = await prisma.tM_Currency.findMany();
+
+  
+    return res.status(200).json({
+      status: "success",
+      data: {
+          currency: currency,
+      },
+    });
+  } catch (err) {
+    console.error("Error searching currency:", err);
+    return next(createError(500, "Internal Server Error"));
+  }
+};
+
+exports.get_contract_docu_SOrder = async (req, res, next) => {
+  try {
+     
+    const contract_docu = await prisma.tM_Contract_Docu.findMany();
+
+  
+    return res.status(200).json({
+      status: "success",
+      data: {
+        contract_docu: contract_docu,
+      },
+    });
+  } catch (err) {
+    console.error("Error searching contract document:", err);
+    return next(createError(500, "Internal Server Error"));
+  }
+};
--- src/routes/sorder-route.js
+++ src/routes/sorder-route.js
@@ -5,4 +5,9 @@ const router = express.Router();
 router.get("/check-sorder", tdSorderController.formLoad);
 router.post("/search-sorder", tdSorderController.Search_SOrder_No_AfterUpdate);
 
+router.get("/customer-info", tdSorderController.get_customer_info_SOrder);
+router.get("/unit", tdSorderController.get_unit_SOrder);
+router.get("/od-quote", tdSorderController.get_od_quote_SOrder);
+router.get("/currency", tdSorderController.get_currency_SOrder);
+router.get("/contract-docu", tdSorderController.get_contract_docu_SOrder);
 module.exports = router;
