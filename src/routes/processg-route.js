05896e513b8c1d1dad1e243522ca114318d98b46
wazwatt_dell <tamkmutnb@gmail.com>
2025-01-09 11:27:05 +0700

    add: get ttProcessG endpoint

--- src/routes/processg-route.js
+++ src/routes/processg-route.js
@@ -5,5 +5,6 @@ const router = express.Router();
 router.get("/fetch-processg", tmProcessGController.fetchProcessG);
 router.put("/update-processg", tmProcessGController.updateProcessg);
 router.get("/fetch-processg-plan", tmProcessGController.Show_action);
+router.get("/fetch-ttprocessg", tmProcessGController.fetchTtProcessG);
 
 module.exports = router;
